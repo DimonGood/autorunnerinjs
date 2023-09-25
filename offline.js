2aa7dbcf93fb9f5f27774f95309e49fb8723779c
DimonGood <110191591+DimonGood@users.noreply.github.com>
2023-09-25 18:08:46 +0300

    gaga pipi penis

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1695653753,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/block.png",
+		"images/player-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
